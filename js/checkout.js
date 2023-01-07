41fc70befe737e83f8c35e40ec6f14f4ee85757e
Xxerd <andre.atalves.09@gmail.com>
2023-01-07 00:29:46 +0000

    finalissima

--- js/checkout.js
+++ js/checkout.js
@@ -37,7 +37,9 @@ $().ready(function () {
         $("#entrega").css({ "background-color": "#FFF3E9", "color": "#000000", "border-color": "#41403F !important", "border": "1px solid", })
         $('#morada').attr("placeholder", "+ Adicionar Estabelecimento")
         console.log(preço)
-        if (preço !== 0) { $('#preço').text((preço - 0.20) + '€') }
+        if (preço != 0) {
+            $('#preço').text((preço - 0.20) + '€')
+        }
 
     })
 
